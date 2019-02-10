dcef567fe7dfd37a0e10f40930eafda2ee4353ce
Aryan Soni <aryansoni1108@gmail.com>
2019-02-10 11:00:40 +0530

    File system module part 2 tutorial done

--- app.js
+++ app.js
@@ -1,44 +1,91 @@
 const fs = require('fs');
-//for create a file
-fs.writeFile('example.txt',"this is an example",(err)=>{
+
+
+
+// making a directory
+fs.mkdir('tutorial',(err)=>{
     if(err){
-        console.log("Error opps");
-    }else{
-        console.log('File successfully created')
-        fs.readFile('example.txt','utf8',(err,file)=>{
+        console.log(err);
+    }
+    else{
+        console.log("succesfully created the file");
+    }
+})
+
+//removing the directory
+fs.mkdir('tutorial',(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        fs.rmdir('tutorial',(err)=>{
             if(err){
                 console.log(err);
-            }else{
-                console.log(file);
+            }
+            else{
+                console.log("Successfully deleted the folder");
             }
         })
     }
 })
 
-//for renaming the file
-fs.rename('example.txt','example2.txt',(err)=>{
-    if(err)
-        console.log(err)
-    else    
-        console.log('successfully renamed the app')
-})
-
-//for apppending a file
-fs.appendFile('example2.txt','Some data being appended',(err)=>{
-    if(err){
-    console.log(err);
-    }
-    else{
-        console.log("appended the data successfully");
-        
-    }
-});
-
-//for deleting the file
-fs.unlink('example2.txt',(err)=>{
+//make file under a directory
+fs.mkdir('tutorial',(err)=>{
     if(err){
         console.log(err);
-    }else{
-        console.log("Success delete");
+    }
+    else{
+        
+        fs.writeFile('./tutorial/example.txt','123',(err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log("sucessfully created file in folder");
+            }
+        })
+    }
+            
+})
+
+//delete the folder
+fs.unlink('./tutorial/example.txt',(err)=>{
+    if(err)
+        console.log(err)
+    else
+        fs.rmdir('tutorial',(err)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log('deleted folder!');
+            }
+        })
+})
+
+// deleting files under folder
+
+fs.readdir('example',(err,files)=>{
+    if(err)
+        console.log(err);
+    else{
+        for(let file of files){
+            fs.unlink('./example/'+file,(err)=>{
+                if(err)
+                    console.log(err);
+                else
+                    console.log('successfuly deleted all the files');
+            })
+        }
     }
 })
+
+
+//Read write streams
+const readStream = fs.createReadStream('./example.txt','utf8');
+const WriteStream = fs.createWriteStream('./example2.txt','utf8');
+readStream.on('data',(chunk)=>{
+    WriteStream.write(chunk)
+})
+
+//Why you should use streams
+// cant use readFile mthod for large file hence we need to use the readable and writable streams
