71e0ad6ed91ac8ac74014a3d6e6906da2fa9710c
Aryan Soni <aryansoni1108@gmail.com>
2019-02-10 18:10:55 +0530

    packagejsonBeginner

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-//simple http code
-// const http = require('http');
-// const server =http.createServer((req,res)=>{
-//     if(req.url==='/'){
-//         res.write('Hello world from nodejs');
-//         res.end();
-//     }else{
-//         res.write('using some other domain');
-//         res.end();
-//     }
-// })
-// server.listen('3000');
-
-//simple http file
-const http = require('http');
-const fs  = require('fs');
-http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/index.html')
-    res.writeHead(200,{'Content-type':'text/html'});//for json content type is application/json
-    readStream.pipe(res);
-})
+//use git init
+const _ = require('lodash');
+let example = _.fill([1,2,3,4,5],"banana",1,4);
+console.log(example);
+//only path update use ~ symbol
+//patch and minor updat use ^ symbol
+//no symbol means use only that version do not update
